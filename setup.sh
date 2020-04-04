0d1900f0eff4416930cb2700e05ba3c1a02b2a30
William Cavalcante <williabreno9@gmail.com>
2020-04-03 23:04:59 -0300

    final

--- setup.sh
+++ setup.sh
@@ -31,7 +31,34 @@ done
 
 wal -R
 
-cd st/st-0.8.2
+echo -e "setup st"
+cd st/
+
+st_mirror=$(cat ./mirror)
+
+mkdir -p build/
+
+cd build/
+
+st_filename=$(basename $st_mirror)
+st_dir=${st_filename%%.tar.gz}
+
+echo -e "downloading st"
+curl $st_mirror -O
+
+tar -xf $(basename $st_mirror)
+
+diffs=$(ls ../*.diff)
+
+cd $st_dir
+
+rm -f config.h
+make clean
+
+for diff in $diffs; do
+	echo -e $diff
+	patch -p1 < "../$diff"
+done
 
 cp ~/.cache/wal/colors-wal-st.h .colors-wal-st.h
 
@@ -42,6 +69,7 @@ awk '/\[256\]/{print gensub(/256/,"257","g");next;}
 /\[257\]/{print gensub(/257/,"256","g");next;}
 {print;} ' < .colors-wal-st.h > colors-wal-st.h
 
-make clean
+cp -f ../../config.h .
+
 sudo make install
 
