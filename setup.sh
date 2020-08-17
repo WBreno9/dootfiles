22cdf7e8614328f3bdd471bdd8364800263d6a28
William Cavalcante <williambreno@mat.ci.ufpb.br>
2020-08-17 18:11:50 -0300

    simplified setup script

--- setup.sh
+++ setup.sh
@@ -7,26 +7,32 @@ EOF
 
 home_files="tmux.conf xinitrc zshrc"
 
-for file in $home_files; do
-	echo -e "creating symlink for $file"
-
-	ln -sf "$(pwd)/home/$file" "$HOME/.$file"
-done
-
 config_files=(
 	'sxhkd/sxhkdrc'
 	'bspwm/bspwmrc'
 	'qutebrowser/autoconfig.yml'
 	'nvim/init.vim'
 	'nvim/coc-settings.json'
-	'./mimeapps.list'
+	'mimeapps.list'
 	)
 
-for file in "${config_files[@]}"; do
-	echo -e "creating symlink for $file"
+declare -a file_pairs
 
-	mkdir -p "$HOME/.config/${file%/*}"
-	ln -sf "$(pwd)/config/$file" "$HOME/.config/$file"
+for file in $home_files; do
+	file_pairs+=("$(pwd)/home/$file $HOME/.$file")
+done
+
+for file in "${config_files[@]}"; do
+	file_pairs+=("$(pwd)config/$file $HOME/.config/$file")
+done
+
+for file_pair in "${file_pairs[@]}"; do
+	pair=($file_pair)
+	mkdir -p $(dirname ${pair[1]})
+
+	echo -e "creating symlink for ${pair[1]}"
+
+	ln -sf ${pair[*]}
 done
 
 # ln -sf /usr/share/zsh/plugins/* $ZSH_CUSTOM/plugins
